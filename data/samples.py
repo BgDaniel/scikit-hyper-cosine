011562780fe964b861af561c217a41882b264c70
Daniel Berger <Daniel.Berger@L-P-A.com>
2020-05-02 13:21:45 +0200

    add samples for 2 dim

--- data/samples.py
+++ data/samples.py
@@ -2,11 +2,9 @@ import pandas as pd
 import numpy as np
 import os
 
-PATH_TO_SIMPLICES = 'simplices.csv'
-
-def get_data(*args, target):
+def get_data(file_name, *args, target):
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    DATA_FILE = os.path.join(dir_path, PATH_TO_SIMPLICES)
+    DATA_FILE = os.path.join(dir_path, file_name)
     samples = pd.read_csv(DATA_FILE, sep=';')
 
     if(args == None):
