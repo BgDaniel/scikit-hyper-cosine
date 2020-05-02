997abc1df3a4ca6b5a18b343879340f7aa26bd30
Daniel Berger <Daniel.Berger@L-P-A.com>
2020-05-02 15:50:00 +0200

    2 dim

--- law_of_cosine_2dim.py
+++ law_of_cosine_2dim.py
@@ -7,16 +7,19 @@ from data import samples as sp
 import matplotlib as mplt
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from data.trigonometric_laws.hyper_triangles import *
+from sklearn import preprocessing
+from matplotlib import pyplot as plt
 
 DEVIATION = 10e-5
+cross_check = False
 
 # Create a Sequential model
 model = Sequential()
-model.add(Dense(256, input_shape=(6,), activation='relu'))
-model.add(Dense(128, activation='relu'))
-model.add(Dense(128, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(1, activation='relu'))
+model.add(Dense(6, input_shape=(3,), activation='sigmoid'))
+model.add(Dense(36, activation='sigmoid'))
+model.add(Dense(6, activation='sigmoid'))
+model.add(Dense(3, activation='sigmoid'))
+model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='mse', optimizer='adam', metrics = ['mse'])
 
 model.summary()
@@ -24,31 +27,57 @@ model.summary()
 X, y = sp.get_data('triangles.csv', 'A', 'B', 'Gamma', target='C')
 
 # cross check data
-for i, _X in enumerate(X):
-    deviation = abs(LAW_OF_COSINE_I3(_X[0], _X[1], _X[2], y[i]) - .0)
-    assert deviation < DEVIATION, 'Deviation too high for triangle number {0}: {1}'.format(i, deviation) 
+if cross_check:
+    for i, _X in enumerate(X):
+        deviation = abs(LAW_OF_COSINE_I3(_X[0], _X[1], _X[2], y[i]) - .0)
+        assert deviation < DEVIATION, 'Deviation too high for triangle number {0}: {1}'.format(i, deviation) 
+
+# scale data to range [0,1]
+X_scaler = preprocessing.MinMaxScaler()
+X_scaler.fit(X)
+X_scaled = X_scaler.transform(X)
+
+y = y.reshape(-1, 1)
+y_scaler = preprocessing.MinMaxScaler()
+y_scaler.fit(y)
+y_scaled = y_scaler.transform(y)
 
 
+# split into traning and test sets
+X_train, X_test, y_train, y_test = ms.train_test_split(X_scaled, y_scaled, test_size = 0.1, random_state=31)
 
-X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size = 0.1, random_state=31)
+# define early stopping by looking at loss function ('mse')
+monitor_val_acc = EarlyStopping(monitor = 'mse', patience=3, min_delta=10e-6)
 
+# train model
+history = model.fit(X_train, y_train, epochs=100, callbacks=[monitor_val_acc], validation_data=(X_test, y_test))
 
-monitor_val_acc = EarlyStopping(monitor = 'val_mse', patience=3)
+# Plot train vs test accuracy/loss during training
+#  "Accuracy"
+plot = False
 
-history = model.fit(X_train, y_train,epochs=200, callbacks=[monitor_val_acc], validation_data=(X_test, y_test))
+if plot:
+    plt.plot(history.history['mse'])    
+    plt.plot(history.history['val_mse'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show(block=False)
+    # "Loss"
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
 
-
+# make prediction
 y_pred = model.predict(X_test)
+y_pred = y_scaler.inverse_transform(y_pred)
 
-# Plot train vs test loss during training
-mplt.plot_loss(history.history['loss'], history.history['val_loss'])
+print(y_pred)
 
-# Plot train vs test accuracy during training
-mplt.plot_accuracy(history.history['acc'], history.history['val_acc'])
 
-accuracy = model.evaluate(X_test, y_test)
-print('Accuracy:', accuracy)
-
-# Evaluate your model 
-#print("Final lost value:",model.evaluate(time_steps, y_positions))
 
